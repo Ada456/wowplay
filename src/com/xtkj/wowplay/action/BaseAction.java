264b47c26227fd7a202845e5d7006a73fe0e18f6
hc19931008 <394306840@qq.com>
2016-07-12 19:52:05 +0800

    Revert "11"

--- src/com/xtkj/wowplay/action/BaseAction.java
+++ src/com/xtkj/wowplay/action/BaseAction.java
@@ -1,40 +1,7 @@
 package com.xtkj.wowplay.action;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.struts2.ServletActionContext;
-
-import com.opensymphony.xwork2.ActionContext;
-
 /**
- * 获取servlet API
- * @author Wangyan
- *
+ * Created by Administrator on 2016/7/11 0011.
  */
 public class BaseAction {
-	
-	public Map getRequest(){
-		return (Map) ActionContext.getContext().get("request");
-	}
-	
-	public Map getSession(){
-		
-		return (Map)ActionContext.getContext().getSession();
-	}
-	
-	public Map getApplication(){
-		
-		return (Map)ActionContext.getContext().getApplication();
-	}
-	
-	public HttpServletResponse getResponse(){
-		
-		HttpServletResponse response=ServletActionContext.getResponse();
-		response.setContentType("text/html;charset=utf-8");
-		response.setCharacterEncoding("utf-8");
-		return response;
-	}
-	
 }
--- src/com/xtkj/wowplay/service/impl/MenuServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.xtkj.wowplay.service.impl;
-
-/**
- * Created by Administrator on 2016/7/12 0012.
- */
-public class MenuServiceImpl {
-}
